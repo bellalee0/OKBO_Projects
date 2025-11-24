2dcffd6e7da5c57242dd7d49dbbb2b3ab0e6983c
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-24 11:13:07 +0900

    feat: 팔로우 API 구현
    Controller
    * 세션 적용 후 주석 처리된 내용으로 변경
    1. 세션에서 로그인한 유저 받기(following)
    2. API Path로 follower의 닉네임 받기
    3. 로그인한 유저의 ID와 입력받은 닉네임 Service로 전달
    
    Service
    1. 로그인한 유저의 ID로 User 객체(following) 생성
    2. 입력받은 닉네임으로 User 객체(follower) 생성
    3. Follow 객체 생성 후, 리포지토리에 저장

--- src/main/java/com/okbo_projects/domain/follow/controller/FollowController.java
+++ src/main/java/com/okbo_projects/domain/follow/controller/FollowController.java
@@ -2,12 +2,24 @@ package com.okbo_projects.domain.follow.controller;
 
 import com.okbo_projects.domain.follow.service.FollowService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/follows")
 @RequiredArgsConstructor
 public class FollowController {
     private final FollowService followService;
+
+    // Follow 관계 create (following: 로그인한 유저 / follower: Path Variable로 입력받은 유저)
+    @PostMapping("/{userId}/{userNickname}")
+    public ResponseEntity<Void> createFollow(
+//            @SessionAttribute(name = "loginUser", required = false) SessionUser sessionUser,
+            @PathVariable Long userId,
+            @PathVariable String userNickname ) {
+//        followService.createFollow(sessionUser.getId(), userNickname);
+        followService.createFollow(userId, userNickname);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
--- src/main/java/com/okbo_projects/domain/follow/service/FollowService.java
+++ src/main/java/com/okbo_projects/domain/follow/service/FollowService.java
@@ -1,6 +1,9 @@
 package com.okbo_projects.domain.follow.service;
 
+import com.okbo_projects.common.entity.Follow;
+import com.okbo_projects.common.entity.User;
 import com.okbo_projects.domain.follow.repository.FollowRepository;
+import com.okbo_projects.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class FollowService {
     private final FollowRepository followRepository;
+    private final UserRepository userRepository;
+
+    // Follow 관계 create (following: 로그인한 유저 / follower: Path Variable로 입력받은 유저)
+    public void createFollow(Long userId, String userNickname) {
+        User following = userRepository.findById(userId)
+                // TODO : 커스텀 예외로 변경
+                .orElseThrow(() -> new IllegalStateException("NOT_FOUND_USER"));
+        User follower = userRepository.findByNickname(userNickname)
+                // TODO : 커스텀 예외로 변경
+                .orElseThrow(() -> new IllegalStateException("NOT_FOUND_USER"));
+        // TODO : following, follower가 같을 때, 이미 형성된 관계일 때 예외 처리
+        Follow follow = new Follow(following, follower);
+        followRepository.save(follow);
+    }
 }
