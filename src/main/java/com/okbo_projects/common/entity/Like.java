e281d166ac5b610338b4d4ff305806b65105d001
윤지현 <139779783+jheony@users.noreply.github.com>
2025-11-26 17:45:23 +0900

    add : like 패키지 복구

---
+++ src/main/java/com/okbo_projects/common/entity/Like.java
@@ -0,0 +1,34 @@
+package com.okbo_projects.common.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "likes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Like extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    public Like(User user, Board board) {
+        this.user = user;
+        this.board = board;
+    }
+}
---
+++ src/main/java/com/okbo_projects/domain/like/service/LikeService.java
@@ -0,0 +1,66 @@
+package com.okbo_projects.domain.like.service;
+
+import com.okbo_projects.common.entity.Board;
+import com.okbo_projects.common.entity.Like;
+import com.okbo_projects.common.entity.User;
+import com.okbo_projects.common.model.SessionUser;
+import com.okbo_projects.domain.board.repository.BoardRepository;
+import com.okbo_projects.domain.like.model.response.BoardLikesCountResponse;
+import com.okbo_projects.domain.like.repository.LikeRepository;
+import com.okbo_projects.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LikeService {
+    private final LikeRepository likeRepository;
+    private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
+
+    // 게시글 좋아요 추가
+    public void addBoardLike(Long boardId, SessionUser sessionUser) {
+
+        Board board = boardRepository.findBoardById(boardId);
+        User user = userRepository.findUserById(sessionUser.getUserId());
+
+        boolean checkLikeExistence = likeRepository.existsByBoardAndUser(board, user);
+        // 예외 처리 추후 수정 예정
+        // 한 게시글에 좋아요 여러번 하려는 경우
+        if (checkLikeExistence) {
+            throw new RuntimeException("이미 좋아요한 게시글입니다.");
+        }
+
+        Like like = new Like(
+                user,
+                board
+        );
+
+        likeRepository.save(like);
+    }
+
+    // 게시글 좋아요 취소
+    public void deleteBoardLike(Long boardId, SessionUser sessionUser) {
+        Board board = boardRepository.findBoardById(boardId);
+        User user = userRepository.findUserById(sessionUser.getUserId());
+
+        boolean checkLikeExistence = likeRepository.existsByBoardAndUser(board, user);
+        // 예외 처리 추후 수정 예정
+        // 좋아요 안했는데 취소하는 경우
+        if (!checkLikeExistence) {
+            throw new RuntimeException("좋아요를 누르지 않은 게시글입니다.");
+        }
+
+        likeRepository.deleteByBoardAndUser(board, user);
+    }
+
+    // 게시글 별 좋아요 개수
+    public BoardLikesCountResponse countBoardLikes(Long boardId) {
+        Board board = boardRepository.findBoardById(boardId);
+        long count = likeRepository.countByBoard(board);
+
+        return new BoardLikesCountResponse(count);
+    }
+}
