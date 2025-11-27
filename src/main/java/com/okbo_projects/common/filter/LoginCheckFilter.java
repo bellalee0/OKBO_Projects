538a747b901bf906e342bfaf6e1758e1698a28f5
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-27 17:19:23 +0900

    feat: LoginCheckFilter에 JWT 적용
    1. 필드 : JwtUtils 추가
    2. doFilterInternal() 수정
      1. 접근한 URL이 whitelist에 있는지 확인 (whitelist에 있다면 바로 통과)
      2. Http Header에 'Bearer '로 시작하는 토큰이 있는지 확인 (없다면 예외 발생)
      3. 토큰이 유효한지 확인 (유효하지 않다면 예외 발생)
      4. request의 Attribute에 SessionUser 객체 만들어 저장
      5. 필터 통과

--- src/main/java/com/okbo_projects/common/filter/LoginCheckFilter.java
+++ src/main/java/com/okbo_projects/common/filter/LoginCheckFilter.java
@@ -2,12 +2,13 @@ package com.okbo_projects.common.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.okbo_projects.common.exception.CustomException;
+import com.okbo_projects.common.model.SessionUser;
 import com.okbo_projects.common.model.response.ErrorResponse;
+import com.okbo_projects.common.utils.JwtUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -27,6 +28,8 @@ import static com.okbo_projects.common.exception.ErrorMessage.UNAUTHORIZED_LOGIN
 @RequiredArgsConstructor
 public class LoginCheckFilter extends OncePerRequestFilter {
 
+    private final JwtUtils jwtUtils;
+
     private static final Map<String, String> whitelist = new HashMap<>();
     static {
         whitelist.put("/users", "POST"); // 회원가입
@@ -43,28 +46,29 @@ public class LoginCheckFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
+        CustomException unauthorizedException = new CustomException(UNAUTHORIZED_LOGIN_REQUIRED);
 
-        if(requestURI.contains("/boards") && request.getMethod().equals("GET")) {
-            if(requestURI.contains("/myboard") || requestURI.contains("/followers")) {
-                HttpSession session = request.getSession(false);
-                if (session == null || session.getAttribute("loginUser") == null) {
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-                    log.error(requestURI + " : 비로그인 접근 시도");
-                    return;
-                }
-            }
-        }
-
-        if(!compareWithWhitelist(requestURI, request.getMethod())) {
-            HttpSession session = request.getSession(false);
-            if (session == null || session.getAttribute("loginUser") == null) {
-                CustomException unauthorizedException = new CustomException(UNAUTHORIZED_LOGIN_REQUIRED);
+        if (compareWithWhitelist(requestURI, request.getMethod())) {
+            filterChain.doFilter(request, response);
+        } else {
+            String authorizationHeader = request.getHeader("Authorization");
+            if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
                 handleCustomException(response, unauthorizedException);
                 log.error("CustomException 발생 : " + unauthorizedException.getErrorMessage().getMessage());
                 return;
             }
+
+            String jwt = authorizationHeader.substring(7);
+            if (!jwtUtils.validateToken(jwt)) {
+                handleCustomException(response, unauthorizedException);
+                log.error("CustomException 발생 : " + unauthorizedException.getErrorMessage().getMessage());
+                return;
+            }
+
+            request.setAttribute("loginUser", new SessionUser(jwtUtils.getUserId(jwt)));
+            filterChain.doFilter(request, response);
         }
-        filterChain.doFilter(request, response);
+
     }
 
     // whitelist와 URI 대조
