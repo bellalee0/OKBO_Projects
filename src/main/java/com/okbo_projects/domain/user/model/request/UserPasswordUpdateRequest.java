7f2f84cc65725d258db6a8deed15c07ea41f7374
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-29 21:51:23 +0900

    feat: 비밀번호 형식 제거
    : 테스트 용이성을 위해 비밀번호 형식 제거

--- src/main/java/com/okbo_projects/domain/user/model/request/UserPasswordUpdateRequest.java
+++ src/main/java/com/okbo_projects/domain/user/model/request/UserPasswordUpdateRequest.java
@@ -12,6 +12,6 @@ public class UserPasswordUpdateRequest {
     @NotBlank
     private String currentPassword;
 
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%])(?=\\S+$).{8,15}$", message = "비밀번호는 영어와 숫자, 특수문자를 최소 1개 이상 포함해서 8~15자리 이내로 입력해주세요.")
+    @NotBlank
     private String newPassword;
 }
